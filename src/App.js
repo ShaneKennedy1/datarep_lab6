5a6737dbba46e810700d88fc9c88b14169e9c93e
unknown <G00393988@atu.ie>
2024-09-30 14:36:31 +0000

    lab complete (no comments)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Content from './Components/contents';
-
-
+import Header from './Components/header';
+import Footer from './Components/footer';
+import NavigationBar from './Components/navigationbar';
 function App() {
   return (
-    <div className="App">
-      <Content></Content>
+    <Router>
+    <div>
+      <NavigationBar/>
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Header</h1>} />
+        <Route path="/create" element={<h1>Footer</h1>} />
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
